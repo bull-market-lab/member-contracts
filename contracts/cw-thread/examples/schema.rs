f0843ebd7cdd8ed543bdaf6d6592963f0cd2e773
llllllluc <58892938+llllllluc@users.noreply.github.com>
2023-10-17 01:30:21 -0700

    nit

--- contracts/cw-thread/examples/schema.rs
+++ contracts/cw-thread/examples/schema.rs
@@ -1,37 +1,10 @@
-// use cosmwasm_schema::write_api;
-// use thread::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-
-// fn main() {
-//     write_api! {
-//         instantiate: InstantiateMsg,
-//         execute: ExecuteMsg,
-//         query: QueryMsg,
-//     }
-// }
-
-use std::env::current_dir;
-use std::fs::create_dir_all;
-
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-// use cosmwasm_std::{CosmosMsg, QueryRequest};
+use cosmwasm_schema::write_api;
 use thread::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-// use resolver::{
-//     condition::Condition, variable::Variable, ExecuteMsg, InstantiateMsg, QueryMsg,
-//     SimulateResponse,
-// };
 
 fn main() {
-    let mut out_dir = current_dir().unwrap();
-    out_dir.push("schema");
-    create_dir_all(&out_dir).unwrap();
-    remove_schemas(&out_dir).unwrap();
-
-    export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    // export_schema(&schema_for!(Variable), &out_dir);
-    // export_schema(&schema_for!(Condition), &out_dir);
-    // export_schema(&schema_for!(SimulateResponse), &out_dir);
-    // export_schema(&schema_for!(CosmosMsg), &out_dir);
-    // export_schema(&schema_for!(QueryRequest<String>), &out_dir);
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
 }
